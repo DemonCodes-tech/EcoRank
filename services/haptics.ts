a36735d5ae996cd18bf7d554292d9282d4c524be
Demon <wadeesshaat@gmail.com>
2026-03-24 09:58:24 +0400

    Delete services directory

--- services/haptics.ts
+++
@@ -1,9 +0,0 @@
-export const vibrate = (pattern: number | number[] = 10) => {
-  if (typeof navigator !== 'undefined' && navigator.vibrate) {
-    navigator.vibrate(pattern);
-  }
-};
-
-export const hapticSuccess = () => vibrate([10, 30, 10]);
-export const hapticError = () => vibrate([50, 30, 50, 30, 50]);
-export const hapticClick = () => vibrate(10);
